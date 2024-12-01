e2bd056cd69d131f77c92e028876be12fc8c8278
Cadde0 <ocaddeo@kth.se>
2024-12-01 18:23:11 +0100

    Finished day 1

--- 1/dec1.py
+++ 1/dec1.py
@@ -5,7 +5,7 @@ list2 = []
 #test_l1 = [3, 4, 2, 1, 3, 3]
 #test_l2 = [4, 3, 5, 3, 9, 3]
 
-data = open('dec1data.txt', 'r')
+data = open('dec1_data.txt', 'r')
 
 for line in data:
     l1, l2 = line.strip().split()
@@ -22,4 +22,17 @@ for i in range(len(list1)):
     dist = abs(list1[i] - list2[i])
     totalDistance += dist
 
-print(totalDistance)
+print("Total Distance: ", totalDistance)
+
+#PART TWO
+
+similarityScore = 0
+
+for x in list1:
+    apperances = 0
+    for i in range(len(list2)):
+        if x == list2[i]:
+            apperances += 1
+    similarityScore += (x * apperances)
+
+print("Similarity Score: ", similarityScore)        
