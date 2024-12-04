9614c3023a77fdb0710760aecdd51b28bb8acd23
Oscar Caddeo <ocaddeo@kth.se>
2024-12-04 22:24:02 +0100

    Horisontal Search day4

--- 4/dec4.py
+++ 4/dec4.py
@@ -1,3 +1,35 @@
 with open("dec4_data.txt", "r") as file:
-    data = file.read()
+    data = file.readlines()
 
+grid = [list(line.strip()) for line in data]
+
+rows = 140
+cols = 140
+
+nof_xmas = 0
+
+# Horisontell sökning
+for row in grid:
+    for start_index in range(137):
+        window = row[start_index: start_index + 4]
+        word = ''.join(window)
+        if word == "XMAS" or word == "SAMX":
+            nof_xmas += 1
+
+
+# Vertikal sökning
+for col in range(140):
+    for start_row in range(137):
+        window = 
+        for i in range(4):
+            window[i] = grid[start_row + i][col]
+        word = ''.join(window)
+        if word == "XMAS" or word == "SAMX":
+            nof_xmas += 1
+
+# Diagonal Sökning // eller flera diagonala, en åt varje håll
+
+
+
+
+print(nof_xmas)
